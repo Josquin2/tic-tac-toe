0df1590d3add2dc93d24555a87f8d96cd3017d97
Josquin2 <Josquinwork@mail.ru>
2024-10-29 20:38:06 +0300

    feat: added pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,14 @@ export default defineNuxtConfig({
   css: ["~/assets/styles/main.scss"],
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
+
   nitro: {
     experimental: {
       websocket: true,
     },
+  },
+  modules: ["@pinia/nuxt"],
+  alias: {
+    pinia: "/node_modules/@pinia/nuxt/node_modules/pinia/dist/pinia.mjs",
   },
 });
